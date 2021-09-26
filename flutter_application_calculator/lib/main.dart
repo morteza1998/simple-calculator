b1fb075ccfe7e08227ed55d1ab218b835458b0de
morteza1998 <m.babolsari@gmail.com>
2021-09-26 17:09:56 +0330

    simple_cal_down

--- flutter_application_calculator/lib/main.dart
+++ flutter_application_calculator/lib/main.dart
@@ -14,13 +14,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
-
 
 
 class HomePage extends StatefulWidget {
@@ -33,100 +31,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    @override
-    
-    dynamic cal =0;
-
-    void numberone(){
-      setState(() {
-        if(cal==0){
-          cal= 1;
-        }else{
-          cal = cal + "1";
-        }
-      });
-    }
-
     return Scaffold(
-      body: Column(
-        children:[
-          
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('simple calculator'),
+        centerTitle: true,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),bottomRight: Radius.circular(40))),
 
-          
-           Container(
-          color: Colors.grey[400],
-          width: size.width,
-          height:size.height*0.23,
-          child: Text(cal.toString(),style: TextStyle(color: Colors.blue,fontSize: 50,fontWeight:FontWeight.w800),),
-          alignment: Alignment.center,
-          
-          
-        ),
-        Expanded(child: Padding(
-          padding: const EdgeInsets.only(left: 10,right: 10,),
-          child: GridView.count(
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 4,
-            children: <Widget> [
-              Component1( Text('%',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('CE',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('C',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Icon(Icons.backspace) , () { },Colors.grey[300]),
-              Component1( Text('1/x',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('x2',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('√',style: TextStyle(fontSize: 30),), () { },Colors.grey[300]),
-              Component1( Text('/',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('7',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('8',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('9',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('x',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('4',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('5',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('6',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('-',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('1',style: TextStyle(fontSize: 30),) , () { numberone(); },Colors.white),
-              Component1( Text('2',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('3',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('+',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
-              Component1( Text('+/-',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('0',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('.',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('=',style: TextStyle(fontSize: 30),) , () { },Colors.blue),
+      ),
+      body: SafeArea( 
+        child: SimpleCalculator(
+          theme: CalculatorThemeData(
 
-            ],
+            borderWidth:12.5 ,
+            borderColor: Colors.grey,
+            expressionStyle: TextStyle(color: Colors.white),
+            commandColor: Colors.grey[600],
+            commandStyle: TextStyle(color: Colors.white,fontSize: 30)
           ),
-        ),)
-        ]
+        ),
       ),
     );
   }
-
-
-
-
-
-
-  // ignore: non_constant_identifier_names
-  Widget Component1(Widget icon , VoidCallback voidCallback,color){
-  return Material(
-    elevation: 20,
-    color: color,
-    borderRadius: BorderRadius.all(Radius.circular(20)),
-    child: IconButton(
-      icon: icon,
-      onPressed: voidCallback,
-
-    ),
-  );
-
 }
-}
-
-
-
 
 
 
