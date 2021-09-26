506f875dde9c627c306b8d66677743f43848e7ed
morteza1998 <m.babolsari@gmail.com>
2021-09-26 16:19:46 +0330

    mycalui

--- flutter_application_calculator/lib/main.dart
+++ flutter_application_calculator/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'dart:math';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,14 +39,24 @@ class _HomePageState extends State<HomePage> {
     
     dynamic cal =0;
 
+    void numberone(){
+      setState(() {
+        if(cal==0){
+          cal= 1;
+        }else{
+          cal = cal + "1";
+        }
+      });
+    }
+
     return Scaffold(
       body: Column(
         children:[
-
+          
 
           
            Container(
-          color: Colors.grey[300],
+          color: Colors.grey[400],
           width: size.width,
           height:size.height*0.23,
           child: Text(cal.toString(),style: TextStyle(color: Colors.blue,fontSize: 50,fontWeight:FontWeight.w800),),
@@ -62,32 +71,26 @@ class _HomePageState extends State<HomePage> {
             mainAxisSpacing: 10,
             crossAxisCount: 4,
             children: <Widget> [
-              Component1( Text('%',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
-              Component1( Text('CE',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
-              Component1( Text('C',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
-              Component1( Icon(Icons.backspace) , () { },Colors.grey[200]),
-              Component1( Text('1/x',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
-              Component1( Text('x2',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
-              Component1( Text('√',style: TextStyle(fontSize: 30),), () { },Colors.grey[200]),
-              Component1( Text('/',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
+              Component1( Text('%',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
+              Component1( Text('CE',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
+              Component1( Text('C',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
+              Component1( Icon(Icons.backspace) , () { },Colors.grey[300]),
+              Component1( Text('1/x',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
+              Component1( Text('x2',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
+              Component1( Text('√',style: TextStyle(fontSize: 30),), () { },Colors.grey[300]),
+              Component1( Text('/',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
               Component1( Text('7',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('8',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('9',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('x',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
+              Component1( Text('x',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
               Component1( Text('4',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('5',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('6',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('-',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
-              Component1( Text('1',style: TextStyle(fontSize: 30),) , () { 
-                setState(() {
-                  if(cal==0){
-                    cal= 1;
-                  }else{cal= cal + '1';};
-                });
-               },Colors.white),
+              Component1( Text('-',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
+              Component1( Text('1',style: TextStyle(fontSize: 30),) , () { numberone(); },Colors.white),
               Component1( Text('2',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('3',style: TextStyle(fontSize: 30),) , () { },Colors.white),
-              Component1( Text('+',style: TextStyle(fontSize: 30),) , () { },Colors.grey[200]),
+              Component1( Text('+',style: TextStyle(fontSize: 30),) , () { },Colors.grey[300]),
               Component1( Text('+/-',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('0',style: TextStyle(fontSize: 30),) , () { },Colors.white),
               Component1( Text('.',style: TextStyle(fontSize: 30),) , () { },Colors.white),
@@ -115,6 +118,7 @@ class _HomePageState extends State<HomePage> {
     child: IconButton(
       icon: icon,
       onPressed: voidCallback,
+
     ),
   );
 
